9a458fe95cca47b25f028e946c5e58ab7b3ac38c
ahmadalshawakri <ashawakri@gmail.com>
2022-04-30 06:08:13 +0300

    Simple Interest Calculator Web Page

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function compute()
     var interest = principal*years*rate/100;
     var year = new Date().getFullYear()+parseInt(years); 
     document.getElementById("result").innerHTML = "If you deposit "
-    +principal+",\<br\> at an interest rate of "+rate+"%\<br\> you will receive an amount of "+interest+
+    +principal+",\<br\> at an interest rate of "+rate+"%\<br\> you will receive an amount of"+interest+
     ",\<br\> in the year "+year+"\<br\>";
 }
 
