d1e9a7bbf659645fda7356b5e4b38306ee676ab4
ahmadalshawakri <ashawakri@gmail.com>
2022-04-30 05:50:38 +0300

    Final Project Simple Interest Calculator

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    if(principal<=0){
+        alert("Enter Positive Number")
+        document.getElementById("principal").focus();
+        return false;
+    }
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal*years*rate/100;
+    var year = new Date().getFullYear()+parseInt(years); 
+    document.getElementById("result").innerHTML = "If you deposit "
+    +principal+",\<br\> at an interest rate of "+rate+"%\<br\> you will receive an amount of "+interest+
+    ",\<br\> in the year "+year+"\<br\>";
+}
+
+function updateRate(){
+    var reteval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = reteval;
 }
         
